6a32a763f414e9df6c0be5954c15747cbc14ba73
naluo10 <na.luo@daimler.com>
2020-08-17 23:10:13 +0800

    main page layout finished

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,92 @@
 <template>
-  <div>
-    Home component
-    <el-button type="info" @click="logout">logout</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../assets/home_log.png" height="58px" width="55px" alt="">
+        <span>SVA</span>
+      </div>
+      <el-button type="info" @click="logout">logout</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          :unique-opened="true"
+          :router="true">
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item :index="'/'+childItem.path + ''" v-for="childItem in item.children" :key="childItem.id">
+              <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>{{ childItem.authName }}</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      menuList: []
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: result } = await this.$http.get('menus')
+      console.log(result)
+      if (result.meta.status !== 200) return this.$message.error(result.meta.msg)
+      this.menuList = result.data
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.home_container{
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0%;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  border-right: 0;
+}
+.el-main {
+  background-color: #eaeaea;
+}
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span>Welcome</span>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
